30f912e67e5dcf8079cd5deab6bfa789b3804f0a
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-12-16 19:06:17 +0530

    error fixes

--- src/globalComponents/Heading.tsx
+++ src/globalComponents/Heading.tsx
@@ -7,14 +7,17 @@ type TextProps = {
 };
 
 export const Heading = ({ children, text, color, ...props }: TextProps) => {
-  const styles = {
-    color: color ? color : "#101124",
-    textTransform: "uppercase",
-    WebkitTextStrokeWidth: "1px",
-    WebkitTextStrokeColor: color === "#101124" ? "#EEEEEE" : "#101124",
-  };
   return (
-    <h1 {...props} style={styles} className="text-7xl absolute">
+    <h1
+      {...props}
+      style={{
+        color: color ? color : "#101124",
+        textTransform: "uppercase",
+        WebkitTextStrokeWidth: "1px",
+        WebkitTextStrokeColor: color === "#101124" ? "#EEEEEE" : "#101124",
+      }}
+      className="text-7xl absolute"
+    >
       {text}
       {children}
     </h1>
